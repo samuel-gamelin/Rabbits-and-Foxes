30785f4e1ebea455eef8dfc9cd69fa57a81f2d28
danihashweh <55706559+danihashweh@users.noreply.github.com>
2019-10-11 20:43:13 -0400

    Update Tile.java
    Implement methods according to the UML Diagram.

--- src/main/java/app/Tile.java
+++ src/main/java/app/Tile.java
@@ -4,13 +4,21 @@
 package app;
 
 /**
- * @author
+ * @author Dani Hashweh
  *
  */
 public class Tile {
-	private Colour tileColour;
-
-	public enum Colour {
+	private Colour tileColour; 
+	private boolean occupied; 
+	private Piece piece; 
+	
+	public Tile() {
+		this.tileColour = null; 
+		this.occupied = false; 
+		this.piece = null; 
+	}
+	
+	public enum Colour{
 		BROWN, GREEN
 	}
 
@@ -18,4 +26,41 @@ public class Tile {
 		this.tileColour = tileColour;
 	}
 
-}
+	public Tile(Colour tileColour, Piece piece) {
+		this.tileColour = tileColour;
+		this.piece = piece; 
+	}
+	
+	public boolean isOccupied() {
+		return occupied; 
+	}
+	
+	private void setOccupied(boolean occupied) {
+		occupied = !occupied; 
+	}
+	
+	public Piece retreivePiece() {
+		return piece; 
+	}
+	
+	public Piece removePiece(Piece piece) {
+		Piece oldPiece = piece; 
+		setOccupied(false); 
+		piece = null; 
+		return oldPiece; 
+	}
+	
+	public void placePiece(Piece piece) {
+		setOccupied(true); 
+		this.piece = piece; 
+	}
+	
+	public Colour getColour() {
+		return tileColour; 
+	}
+	
+	@Override
+	public String toString() {
+		return ""; 
+	}
+	}
