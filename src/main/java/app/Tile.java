aeee8b850ae9d03cd5a255fa85854c59b952a196
danihashweh <55706559+danihashweh@users.noreply.github.com>
2019-10-11 23:15:00 -0400

    Update Tile.java
    Add javadoc to Tile Class.

--- src/main/java/app/Tile.java
+++ src/main/java/app/Tile.java
@@ -8,41 +8,77 @@ package app;
  *
  */
 public class Tile {
-	private Colour tileColour; 
+	/**
+	 * A Colour that will represent the colour of the tile. 
+	 */
+	private Colour tileColour;
+	/**
+	 * A boolean that will represent if the tile is occupied or not. 
+	 */
 	private boolean occupied; 
+	/**
+	 * A piece of type Piece that will be placed on the tile. 
+	 */
 	private Piece piece; 
 	
+	/**
+	 * Sets the tileColour, occupied, and piece to the default values. 
+	 */
 	public Tile() {
 		this.tileColour = null; 
 		this.occupied = false; 
 		this.piece = null; 
 	}
 	
-	public enum Colour{
-		BROWN, GREEN
-	}
-
+	/**
+	 * Constructs the Tile based on the passed tileColour. 
+	 * @param tileColour The colour of the tile as type Colour.  
+	 */
 	public Tile(Colour tileColour) {
 		this.tileColour = tileColour;
 	}
 
+	/**
+	 * Constructs the Tile based on the tileColour and the piece which will be placed on the tile. 
+	 * @param tileColour The colour of the Tile as type Colour. 
+	 * @param piece The piece type Piece that will be placed on the Tile. 
+	 */
 	public Tile(Colour tileColour, Piece piece) {
 		this.tileColour = tileColour;
 		this.piece = piece; 
 	}
 	
+	public enum Colour{
+		BROWN, GREEN
+	}
+	
+	/**
+	 * @return True if the Tile is occupied, False if the Tile is not occupied. 
+	 */
 	public boolean isOccupied() {
 		return occupied; 
 	}
 	
+	/**
+	 * Sets the tile to the stated occupied state. 
+	 * @param occupied If True, Tile will become occupied, if False, the Tile will become available.
+	 */
 	private void setOccupied(boolean occupied) {
 		occupied = !occupied; 
 	}
 	
+	/**
+	 * @return the current Piece placed on the Tile. 
+	 */
 	public Piece retreivePiece() {
 		return piece; 
 	}
 	
+	/**
+	 * Removes piece from the Tile. 
+	 * @param piece The piece of type Piece that will removed from the tile. 
+	 * @return The piece that was removed. 
+	 */
 	public Piece removePiece(Piece piece) {
 		Piece oldPiece = piece; 
 		setOccupied(false); 
@@ -50,17 +86,27 @@ public class Tile {
 		return oldPiece; 
 	}
 	
+	/**
+	 * Places piece and sets occupied to true
+	 * @param piece Of type Piece will now be placed on the tile. 
+	 */
 	public void placePiece(Piece piece) {
 		setOccupied(true); 
 		this.piece = piece; 
 	}
 	
+	/**
+	 * @return The colour of the tile
+	 */
 	public Colour getColour() {
 		return tileColour; 
 	}
 	
+	/**
+	 * @return A string representation
+	 */
 	@Override
 	public String toString() {
-		return ""; 
+		return piece.toShortString(); 
 	}
 	}
