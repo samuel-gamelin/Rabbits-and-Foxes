4f8829f0023a8431350254ed48c49dd214edb2c6
Mohamed Radwan <mohamed.rad@outlook.com>
2019-10-14 23:33:19 -0400

    updated the game class, the uml and added a new picture for the uml

--- src/main/java/app/Game.java
+++ src/main/java/app/Game.java
@@ -5,7 +5,7 @@ package app;
 
 /**
  * This class is responsible for controlling the game. Using parser and board
- * class to play the game/
+ * class to play the game
  * 
  * @author Mohamed Radwan
  * @version 1.0
@@ -20,7 +20,75 @@ public class Game {
 	 */
 	public Game() {
 		this.parser = new Parser();
+	}
+
+	/***
+	 * This method will start the game but wait for the user to type start to play
+	 * the game. It then calls the method playGame()
+	 */
+	public void startGame() {
 		this.board = new Board();
+		System.out.println("Welcome to JumpIN Game !");
+		System.out.println("Type 'help' if you need help.");
+		System.out.println("\nPlease type 'Start' to start the game. Have fun!");
+		boolean startGame = false;
+		do {
+			if (parser.readCommand().getCommandWord() == CommandWord.START) {
+				startGame = true;
+			}
+
+		} while (!startGame);
+		System.out.println("Get Ready, Game is Starting.");
+		System.out.println("Foxes are moving to position...");
+		System.out.println("Game started, have fun.");
+		this.playGame();
+	}
+
+	/***
+	 * This method will run the game for the user to interact with.
+	 */
+	private void playGame() {
+		boolean done = false;
+		while (!(this.isDone() && done)) {
+			Command command = parser.readCommand();
+			CommandWord commandWord = command.getCommandWord();
+			done = this.processCommandWord(commandWord);
+		}
+		System.out.println("\nThe foxes will now go back to sleep!");
+		System.out.println("Thank you for playing. Good bye.");
+	}
+
+	/***
+	 * Process the command to see how the game should respond to the user's input.
+	 * 
+	 * @param command
+	 * @return
+	 */
+	private boolean processCommandWord(CommandWord commandWord) {
+		boolean endGame = false;
+		if (commandWord == CommandWord.HELP) {
+			System.out.println("Here is the help menu:");
+			parser.showAllCommands();
+			System.out.println();
+		} else if (commandWord == CommandWord.MOVE) {
+			this.movePiece(commandWord);
+		} else if (commandWord == CommandWord.INVALID) {
+			System.out.println("Invalid command please try again.\nType 'help' if you need help.\n");
+		} else if (commandWord == CommandWord.QUIT) {
+			return true;
+		} else if (commandWord == CommandWord.RESET) {
+			this.startGame();
+		}
+		return endGame;
+	}
+
+	/***
+	 * Invokes a method in board to move the piece on specific coordinate
+	 * 
+	 * @param command
+	 */
+	private void movePiece(CommandWord command) {
+		System.out.println("Move Piece ... TO DO");
 	}
 
 	/***
@@ -34,81 +102,14 @@ public class Game {
 	}
 
 	/***
-	 * This method is used to play the game.
-	 */
-	public void playGame() {
-		System.out.println("Welcome to JumpIN Game !");
-		System.out.println("Type 'help' if you need help.\n");
-		boolean done = false;
-		while (!this.isDone()) {
-			command = parser.readCommand();
-			done = this.processCommandWord(command);
-		}
-		System.out.println("\nThank you for playing. Good bye.");
-	}
-
-	/***
-	 * Prints all the commands that can be used within the game.
-	 */
-	private void printHelp() {
-		System.out.println("Here is the help menu:");
-		parser.showCommands();
-	}
-
-	/***
-	 * Process the command to see how the game should respond.
-	 * 
-	 * @param command
-	 * @return
-	 */
-	private boolean processCommandWord(CommandWord command) {
-		boolean endGame = false;
-		CommandWord commandWord = command.readCommand();
-		if (commandWord == CommandWord.HELP) {
-			this.printHelp();
-		} else if (commandWord == CommandWord.MOVE) {
-			this.movePiece(commandWord);
-		} else if (commandWord == CommandWord.INVALID) {
-			System.out.println("Invalid command please try again.");
-			System.out.println("Type 'help' if you need help.\n");
-		} else if (commandWord == CommandWord.QUIT) {
-			this.quitGame(commandWord);
-		} else if (commandWord == CommandWord.START) {
-
-		}
-		return endGame;
-	}
-
-	/***
-	 * Invokes a method in board to move the piece on specific coordinate
-	 * 
-	 * @param command
-	 */
-	private void movePiece(CommandWord command) {
-
-	}
-
-	/***
-	 * checks if the user truly wants to quite the game.
-	 * 
-	 * @param commandWord
-	 * @return true if the user wants to quite the game
-	 */
-	private boolean quitGame(CommandWord commandWord) {
-		// TO DO
-		return true;
-	}
-
-	/***
 	 * Main method used to run the game
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// create a new game
 		Game game = new Game();
-		// run the game
-		game.playGame();
+		game.startGame();
+
 	}
 
 }
