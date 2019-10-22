a7b8a465566c933a149d11e23656e81bf144b64d
Mohamed Radwan <mohamed.rad@outlook.com>
2019-10-22 19:51:56 -0400

    added view and controller

---
+++ src/main/java/view/GameView.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class GameView extends JFrame implements Observer{
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
