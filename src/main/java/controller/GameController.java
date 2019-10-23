8de5268e062cf3ac0aeafb2c493254572d6205fc
Mohamed Radwan <mohamed.rad@outlook.com>
2019-10-23 13:21:58 -0400

    separate view from controller

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import model.BoardEvent;
+import model.BoardListener;
+
+/**
+ * @author Mohamed
+ *
+ */
+public class GameController implements BoardListener {
+
+	@Override
+	public void handleBoardEvent(BoardEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -5,12 +5,6 @@ import javax.swing.JFrame;
 import model.BoardEvent;
 import model.BoardListener;
 
-public class GameView extends JFrame implements BoardListener {
-	private static final long serialVersionUID = -8631293984099714762L;
+public class GameView extends JFrame {
 
-	@Override
-	public void handleBoardEvent(BoardEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 }
