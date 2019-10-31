9ed179e59b31f38e3a9f0b855354fc92cdda37d4
danihashweh <55706559+danihashweh@users.noreply.github.com>
2019-10-31 11:24:37 -0400

    Update GameView.java
    - Made it so btnHelp and menuHelp use are in the same spot in actionPerformed instead of having 2 lambda functions.
    
    - Same with btnQuit and menuQuit.
    
    - Organized the code, so all of the gameFrame items are together, and all the mainMenuFrame items are together.

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -19,14 +19,13 @@ import resources.Resources;
  * the game.
  * 
  * @author Dani Hashweh
- * @author Mohamed Radwan
  * @author Samuel Gamelin
  * @version 2.0
  */
-public class GameView extends JFrame implements BoardListener {
+public class GameView extends JFrame implements BoardListener, ActionListener {
 	private static final long serialVersionUID = 1L;
-	private JFrame mainMenuFrame; 
-	private JFrame gameFrame; 
+	private JFrame mainMenuFrame;
+	private JFrame gameFrame;
 
 	private JMenuBar menuBar;
 
@@ -34,58 +33,59 @@ public class GameView extends JFrame implements BoardListener {
 	private JMenuItem menuQuit;
 	private JMenuItem menuHelp;
 
+	private JButton btnStart;
+	private JButton btnHelp;
+	private JButton btnQuit;
+
 	private GridLayout boardLayout;
 
 	private JButton[][] buttons;
 
 	private Board board;
-	private Piece piece;
 
 	private GameController gameController;
-	
+
 	private void initialize() {
-		mainMenuFrame = new JFrame(); 
-		gameFrame = new JFrame(); 
 		board = new Board();
 		board.addListener(this);
-
 		gameController = new GameController(board);
+
+		mainMenuFrame = new JFrame();
+
+		mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// absolute layout
+		mainMenuFrame.getContentPane().setLayout(null);
+		mainMenuFrame.setBounds(100, 100, 875, 925);
+		setResizable(false);
+		mainMenuFrame.setLocationRelativeTo(null);
+
+		btnStart = new JButton("Start");
+		btnStart.setBounds(320, 249, 214, 83);
+		mainMenuFrame.getContentPane().add(btnStart);
+
+		btnHelp = new JButton("Help");
+		btnHelp.setBounds(320, 375, 214, 83);
+		mainMenuFrame.getContentPane().add(btnHelp);
+
+		btnQuit = new JButton("Quit");
+		btnQuit.setBounds(320, 517, 214, 83);
+		mainMenuFrame.getContentPane().add(btnQuit);
+
+		gameFrame = new JFrame();
 
 		gameFrame.setContentPane(new JLabel(Resources.BOARD));
 		gameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		gameFrame.setSize(875, 925);
 		setResizable(false);
 		gameFrame.setLocationRelativeTo(null);
-		
-		mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//absolute layout
-		mainMenuFrame.getContentPane().setLayout(null);
-		mainMenuFrame.setBounds(100, 100, 875, 925);
-		setResizable(false);
-		mainMenuFrame.setLocationRelativeTo(null);
-		
-		JButton btnStart = new JButton("Start");
-		btnStart.setBounds(320, 249, 214, 83);
-		mainMenuFrame.getContentPane().add(btnStart);
-		
-		JButton btnHelp = new JButton("Help");
-		btnHelp.setBounds(320, 375, 214, 83);
-		mainMenuFrame.getContentPane().add(btnHelp);
-		
-		JButton btnQuit = new JButton("Quit");
-		btnQuit.setBounds(320, 517, 214, 83);
-		mainMenuFrame.getContentPane().add(btnQuit);
-		
-		menuBar = new JMenuBar();
 
+		menuBar = new JMenuBar();
 		menuReset = new JMenuItem("Reset");
 		menuHelp = new JMenuItem("Help");
 		menuQuit = new JMenuItem("Quit");
-
 		menuBar.add(menuReset);
 		menuBar.add(menuHelp);
 		menuBar.add(menuQuit);
-
 		gameFrame.setJMenuBar(menuBar);
 
 		boardLayout = new GridLayout(5, 5);
@@ -96,7 +96,7 @@ public class GameView extends JFrame implements BoardListener {
 		for (int i = 0; i < Board.SIZE; i++) {
 			for (int j = 0; j < Board.SIZE; j++) {
 				buttons[i][j] = new JButton();
-				piece = board.getPiece(i, j);
+				Piece piece = board.getPiece(i, j);
 				if ((piece != null)) {
 					if ((piece.getPieceType()).equals(Piece.PieceType.MUSHROOM))
 						(buttons[i][j]).setIcon(Resources.MUSHROOM);
@@ -121,30 +121,15 @@ public class GameView extends JFrame implements BoardListener {
 				gameFrame.add(buttons[i][j]);
 			}
 		}
-		
+
+		btnHelp.addActionListener(this);
+		menuHelp.addActionListener(this);
+		btnQuit.addActionListener(this);
+		menuQuit.addActionListener(this);
+
 		btnStart.addActionListener(e -> {
 			mainMenuFrame.dispose();
-			gameFrame.setVisible(true);		
-		});
-		
-		btnHelp.addActionListener(e -> {
-			 helpText();
-		});
-		
-		menuHelp.addActionListener(e -> {
-			 helpText();
-		});
-
-		menuQuit.addActionListener(e -> {
-			if (JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Exit Rabbit and Foxes!",
-					JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
-				System.exit(0);
-		});
-
-		
-		
-		menuHelp.addActionListener(e -> {
-			
+			gameFrame.setVisible(true);
 		});
 
 	}
@@ -153,7 +138,7 @@ public class GameView extends JFrame implements BoardListener {
 	 * Create the application gui
 	 */
 	public GameView() {
-		initialize(); 
+		initialize();
 	}
 
 	public static void main(String[] args) {
@@ -165,16 +150,24 @@ public class GameView extends JFrame implements BoardListener {
 			}
 		});
 	}
-	
-	private void helpText() {
-		JOptionPane.showMessageDialog(null,
-				"Start: Starts the game. \n"
-						+ "Pause: Pauses the game, and clicking it again, will resume the game. \n"
-						+ "Reset: Restarts the game. \n" + "Quit: Exits the application");
-	}
 
 	@Override
 	public void handleBoardEvent(BoardEvent e) {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnHelp || e.getSource() == menuHelp) {
+			JOptionPane.showMessageDialog(null,
+					"Start: Starts the game. \n"
+							+ "Pause: Pauses the game, and clicking it again, will resume the game. \n"
+							+ "Reset: Restarts the game. \n" + "Quit: Exits the application");
+		} else if (e.getSource() == btnQuit || e.getSource() == menuQuit) {
+			if (JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Exit Rabbit and Foxes!",
+					JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
+				System.exit(0);
+		}
 
 	}
 }
