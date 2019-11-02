2498240efbab105c12389eadcf2740a0200db6a7
danihashweh <55706559+danihashweh@users.noreply.github.com>
2019-11-02 16:51:08 -0400

    Update GameView.java
    - Refactor of code
    - Keystrokes to help menu added
    - Menubaritems replaced with buttons

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,18 +1,32 @@
 package view;
 
-import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 import controller.GameController;
 import model.Board;
 import model.BoardListener;
 import model.Fox;
+import model.Fox.Direction;
+import model.Mushroom;
 import model.Piece;
 import model.Rabbit;
 import resources.Resources;
@@ -26,16 +40,12 @@ import resources.Resources;
  * @version 2.0
  */
 public class GameView implements BoardListener, ActionListener {
-	private double sideLength = 0.75 * Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-	
 	private JFrame mainMenuFrame;
 	private JFrame gameFrame;
 
-	private JMenuBar menuBar;
-
-	private JMenuItem menuReset;
-	private JMenuItem menuQuit;
-	private JMenuItem menuHelp;
+	private JButton menuReset;
+	private JButton menuHelp;
+	private JButton menuQuit;
 
 	private JButton btnStart;
 	private JButton btnHelp;
@@ -47,13 +57,12 @@ public class GameView implements BoardListener, ActionListener {
 
 	private GameController gameController;
 
-	private int[] xy;
-
 	/**
-	 * Create the application gui
+	 * Creates the application GUI.
 	 */
 	public GameView() {
-		// Forces the look and feel of the application to remain consistent across platforms
+		// Forces the look and feel of the application to remain consistent across
+		// platforms
 		try {
 			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 		} catch (Exception e) {
@@ -66,11 +75,11 @@ public class GameView implements BoardListener, ActionListener {
 		 * 
 		 */
 		mainMenuFrame = new JFrame("Rabbit and Foxes!");
-		
+
 		// Box Layout for main menu
 		Container mainMenuPane = mainMenuFrame.getContentPane();
 		mainMenuPane.setLayout(new BoxLayout(mainMenuPane, BoxLayout.Y_AXIS));
-		
+
 		btnStart = new JButton("Start");
 		btnHelp = new JButton("Help");
 		btnQuit = new JButton("Quit");
@@ -79,7 +88,7 @@ public class GameView implements BoardListener, ActionListener {
 		addMenuButton(mainMenuPane, btnQuit);
 
 		mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainMenuFrame.setSize((int) sideLength, (int) sideLength);
+		mainMenuFrame.setSize((int) Resources.SIDE_LENGTH, (int) Resources.SIDE_LENGTH);
 		mainMenuFrame.setResizable(false);
 		mainMenuFrame.setLocationRelativeTo(null);
 		mainMenuFrame.setVisible(true);
@@ -90,18 +99,16 @@ public class GameView implements BoardListener, ActionListener {
 		 * 
 		 */
 		gameFrame = new JFrame("Rabbit and Foxes!");
-
-		menuBar = new JMenuBar();
-		menuReset = new JMenuItem("Reset");
-		menuHelp = new JMenuItem("Help");
-		menuQuit = new JMenuItem("Quit");
-		menuBar.add(menuReset);
-		menuBar.add(menuHelp);
-		menuBar.add(menuQuit);
-		
 		// BorderLayout for game frame
 		Container gamePane = gameFrame.getContentPane();
 		gamePane.setLayout(new BorderLayout());
+
+		// Menu bar
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(menuReset = createMenuBarButton("Reset"));
+		menuBar.add(menuHelp = createMenuBarButton("Help"));
+		menuBar.add(menuQuit = createMenuBarButton("Quit"));
+
 		gamePane.add(menuBar, BorderLayout.NORTH);
 
 		// GridLayout for board frame
@@ -109,17 +116,17 @@ public class GameView implements BoardListener, ActionListener {
 		boardLabel.setLayout(new GridLayout(5, 5));
 		gamePane.add(boardLabel, BorderLayout.CENTER);
 
+		// Organize the game frame
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameFrame.setResizable(false);
 		gameFrame.pack();
 		gameFrame.setLocationRelativeTo(null);
 
-		/**
-		 * 
-		 */
+		// Create the board and controller
 		(board = new Board()).addListener(this);
 		gameController = new GameController(board);
 
+		// Create all buttons
 		buttons = new JButton[5][5];
 
 		for (int i = 0; i < Board.SIZE; i++) {
@@ -130,7 +137,14 @@ public class GameView implements BoardListener, ActionListener {
 				buttons[j][i].setContentAreaFilled(false);
 				buttons[j][i].setFocusPainted(false);
 				boardLabel.add(buttons[j][i]);
-				buttons[j][i].addActionListener(this);
+
+				// Register an anonymous listener on the button which notifies the controller
+				// whenever a move is made (i.e. a button is clicked)
+				final int x = j;
+				final int y = i;
+				buttons[j][i].addActionListener(e -> {
+					gameController.registerMove(x, y);
+				});
 			}
 		}
 
@@ -141,26 +155,48 @@ public class GameView implements BoardListener, ActionListener {
 		menuHelp.addActionListener(this);
 		btnQuit.addActionListener(this);
 		menuQuit.addActionListener(this);
-		
+
 		btnStart.addActionListener(e -> {
 			mainMenuFrame.dispose();
 			gameFrame.setVisible(true);
 		});
-
-		xy = new int[2];
 	}
 
 	/**
 	 * Adds a button to the specified pane. Used in building the menu.
 	 * 
-	 * @param pane The pane to which to add the specified button
+	 * @param pane   The pane to which to add the specified button
 	 * @param button The button to add
 	 */
 	private void addMenuButton(Container pane, JButton button) {
-		pane.add(Box.createRigidArea(new Dimension(0, (int) (0.5 * sideLength / 4))));
-		button.setMaximumSize(new Dimension((int) sideLength / 2, (int) (0.15 * sideLength)));
+		pane.add(Box.createRigidArea(new Dimension(0, (int) (Resources.SIDE_LENGTH / 8))));
+		button.setMaximumSize(new Dimension((int) Resources.SIDE_LENGTH / 2, (int) (0.15 * Resources.SIDE_LENGTH)));
 		button.setAlignmentX(Component.CENTER_ALIGNMENT);
 		pane.add(button);
+	}
+
+	/**
+	 * Creates and returns a JButton suitable for the game's menu bar.
+	 * 
+	 * @param text The text inside the button
+	 * @return The newly created JButton
+	 */
+	private JButton createMenuBarButton(String text) {
+		JButton button = new JButton("<html><p style='text-align:center;'>" + text + "</p></html>");
+		button.setFocusPainted(false);
+		button.setOpaque(false);
+		button.setContentAreaFilled(false);
+		button.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+				.put(KeyStroke.getKeyStroke(Character.toLowerCase(text.charAt(0))), text);
+		button.getActionMap().put(text, new AbstractAction() {
+			private static final long serialVersionUID = -3061675265827831267L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				button.doClick();
+			}
+		});
+		return button;
 	}
 
 	/**
@@ -170,60 +206,56 @@ public class GameView implements BoardListener, ActionListener {
 		for (int i = 0; i < Board.SIZE; i++) {
 			for (int j = 0; j < Board.SIZE; j++) {
 				Piece piece = board.getPiece(i, j);
-				if ((piece != null)) {
-					if ((piece.getPieceType()).equals(Piece.PieceType.MUSHROOM))
+				if (piece != null) {
+					if (piece instanceof Mushroom) {
 						(buttons[i][j]).setIcon(Resources.MUSHROOM);
-					else if ((piece.getPieceType()).equals(Piece.PieceType.RABBIT)) {
-						if (((Rabbit) (piece)).isColour())
+					} else if (piece instanceof Rabbit) {
+						if (((Rabbit) (piece)).isColour()) {
 							(buttons[i][j]).setIcon(Resources.RABBIT1);
-						else
+						} else {
 							(buttons[i][j]).setIcon(Resources.RABBIT2);
-					} else if ((piece.getPieceType()).equals(Piece.PieceType.FOX)) {
-						if (((Fox) (piece)).getDirection().equals(Fox.Direction.UP)) {
-							if (((Fox) (piece)).getFoxType() == (Fox.FoxType.HEAD))
+						}
+					} else if (piece instanceof Fox) {
+						Direction direction = ((Fox) (piece)).getDirection();
+						boolean isHead = ((Fox) (piece)).getFoxType().equals(Fox.FoxType.HEAD);
+						if (direction.equals(Fox.Direction.UP)) {
+							if (isHead) {
 								(buttons[i][j]).setIcon(Resources.FOX_HEAD_UP);
-							else
+							} else {
 								(buttons[i][j]).setIcon(Resources.FOX_TAIL_UP);
-						} else if (((Fox) (piece)).getDirection().equals(Fox.Direction.DOWN))
-							if (((Fox) (piece)).getFoxType() == (Fox.FoxType.HEAD))
-								(buttons[i][j]).setIcon(Resources.FOX_HEAD_UP);
-							else
-								(buttons[i][j]).setIcon(Resources.FOX_TAIL_UP);
-						else if (((Fox) (piece)).getDirection().equals(Fox.Direction.LEFT))
-							if (((Fox) (piece)).getFoxType() == (Fox.FoxType.HEAD))
+							}
+						} else if (direction.equals(Fox.Direction.DOWN))
+							if (isHead) {
+								(buttons[i][j]).setIcon(Resources.FOX_HEAD_DOWN);
+							} else {
+								(buttons[i][j]).setIcon(Resources.FOX_TAIL_DOWN);
+							}
+						else if (direction.equals(Fox.Direction.LEFT))
+							if (isHead) {
 								(buttons[i][j]).setIcon(Resources.FOX_HEAD_LEFT);
-							else
+							} else {
 								(buttons[i][j]).setIcon(Resources.FOX_TAIL_LEFT);
-						else if (((Fox) (piece)).getDirection().equals(Fox.Direction.RIGHT))
-							if (((Fox) (piece)).getFoxType() == (Fox.FoxType.HEAD))
+							}
+						else if (direction.equals(Fox.Direction.RIGHT))
+							if (isHead) {
 								(buttons[i][j]).setIcon(Resources.FOX_HEAD_RIGHT);
-							else
+							} else {
 								(buttons[i][j]).setIcon(Resources.FOX_TAIL_RIGHT);
+							}
 					}
 				} else {
 					buttons[i][j].setIcon(null);
-					buttons[i][j].revalidate();
+					//buttons[i][j].revalidate();
 				}
 			}
 		}
 	}
 
 	/**
+	 * Enables or disables all buttons, depending on the specified state.
 	 * 
-	 * @param c
+	 * @param state The state in which all buttons should be set
 	 */
-	private void locateButton(Object b) {
-		for (int x = 0; x < buttons.length; x++) {
-			for (int y = 0; y < buttons.length; y++) {
-				if (b.equals(buttons[x][y])) {
-					xy[0] = x;
-					xy[1] = y;
-				}
-			}
-		}
-	}
-
-	//i will add javadoc, false if u want all buttons disabled, true if enabled 
 	private void gameEndBoard(boolean state) {
 		for (int i = 0; i < Board.SIZE; i++) {
 			for (int j = 0; j < Board.SIZE; j++) {
@@ -231,23 +263,24 @@ public class GameView implements BoardListener, ActionListener {
 			}
 		}
 	}
-	
-	//will add javadoc, this basically resets the game once the game is won
-	//eventually when we have levels, they will be notified here. 
-	private void gameWinReset(Board board) {
+
+	/**
+	 * Resets the game.
+	 */
+	private void gameWinReset() {
 		(this.board = gameController.reset()).addListener(this);
 		updateView();
 		gameEndBoard(true);
 	}
 
 	/**
-	 * 
+	 * Handles button input for the menus.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == btnHelp || e.getSource() == menuHelp) {
-			JOptionPane.showMessageDialog(null,
-					"Start: Starts the game. \n" + "Reset: Restarts the game. \n" + "Quit: Exits the application");
+			JOptionPane.showMessageDialog(null, "Start (s): Starts the game. \n" + "Reset (r): Restarts the game. \n"
+					+ "Quit (q): Exits the application", "Help", JOptionPane.INFORMATION_MESSAGE);
 		} else if (e.getSource() == btnQuit || e.getSource() == menuQuit) {
 			if (JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Exit Rabbit and Foxes!",
 					JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
@@ -256,11 +289,8 @@ public class GameView implements BoardListener, ActionListener {
 			if (JOptionPane.showConfirmDialog(null,
 					"Are you sure you want to reset the game? (Your progress will be lost)", "Reset Rabbit and Foxes!",
 					JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-				gameWinReset(this.board); 
+				gameWinReset();
 			}
-		} else if (e.getSource().getClass().equals(JButton.class)) {
-			locateButton(e.getSource());
-			gameController.registerMove(xy[0], xy[1]);
 		}
 	}
 
@@ -272,13 +302,14 @@ public class GameView implements BoardListener, ActionListener {
 	public void handleBoardChange() {
 		updateView();
 		if (board.isInWinningState()) {
+			gameEndBoard(false);
 			JOptionPane.showMessageDialog(null, "Congrats! You win!");
-			gameWinReset(board); 
+			gameWinReset();
 		}
 	}
 
 	/**
-	 * Starts the Rabbits and Foxes game
+	 * Starts the Rabbits and Foxes game.
 	 * 
 	 * @param args The command-line arguments
 	 */
