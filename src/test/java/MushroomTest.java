d7294b4903f36b040a36da146026ced92036722d
Abdoltim <a.elnakla@hotmail.com>
2019-11-03 03:50:44 -0500

    Added More Tests
    Tests added for Rabbit and Mushroom

--- src/test/java/MushroomTest.java
+++ src/test/java/MushroomTest.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import model.Board;
+import model.Move;
 import model.Mushroom;
 import model.Piece.PieceType;
 
@@ -20,7 +22,9 @@ public class MushroomTest {
 	
 	@Test
 	public void testMove() {
-		assertFalse(mushroom.move(null, null));
+		Move move = new Move(1,2,3,4);
+		Board board = new Board();
+		assertFalse(mushroom.move(move, board));
 	}
 
 }
