ee839d8cd600b0045018a9ef209dd8c6d7aa2c0f
Samuel Gamelin <gamelinsamuel@gmail.com>
2019-11-09 00:49:51 -0500

    Update Solver.java

--- src/main/java/util/Solver.java
+++ src/main/java/util/Solver.java
@@ -1,6 +1,6 @@
 package util;
 
-import java.util.Set;
+import java.util.List;
 
 import model.Board;
 
@@ -14,14 +14,20 @@ import model.Board;
  * @version 3.0
  */
 public class Solver {
-
-	private static Move extractMove(Set<Node> list) {
-		return null;
-	}
-
+	/**
+	 * Determines the next best move, given a board object.
+	 * 
+	 * @param board The board whose next best move is to be determined
+	 * @return The next best move. Should there be no next best move, a move object with an invalid direction is returned.
+	 */
 	public static Move getNextBestMove(Board board) {
 		Graph graph = new Graph();
-		return extractMove(graph.depthFirstSearch(new Node(board)));
+		List<Node> winningNodePath = graph.breadthFirstSearch(new Node(board));
+		
+		if (winningNodePath.size() < 2) {
+			return new Move(-1, -1, -1, -1);
+		}
+		
+		return winningNodePath.get(0).getMoveTo(winningNodePath.get(1));
 	}
-
 }
